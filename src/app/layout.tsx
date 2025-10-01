2fccefcfd83e49a413e105ac290fe39f4e9e3f91
duarteeigor <duarteigor562@gmail.com>
2025-10-01 10:03:51 -0300

    feat: add header component on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Inter({
   variable: "--font-inter",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
       </body>
     </html>
