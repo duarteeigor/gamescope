b69fdd1ac82405bbc50ed27fbcbb7ba40cdce6dd
duarteeigor <duarteigor562@gmail.com>
2025-10-01 12:28:02 -0300

    feat: creating card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Image from "next/image"
+
+interface CardProps {
+  image: string,
+  title: string,
+  gen: string,
+  plat?: string,
+  rating?: number
+  lancamento?: string
+}
+export default function Card({ image, title, gen, plat, rating, lancamento }: CardProps) {
+  return (
+    <div className="bg-[#1E1E1E] rounded-xl shadow-lg overflow-hidden w-64 hover:scale-105 transition-transform duration-300">
+      <div className="relative flex flex-1 w-full  h-55">
+        <Image
+          src={image}
+          alt={title}
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <div className="p-4 flex flex-col h-40  gap-3">
+        <h3 className="text-white font-semibold text-lg line-clamp-2">{title}</h3>
+        <div className="flex justify-between">
+          <p className="text-white font-light text-sm">{gen}</p>
+          <p className="text-white font-light text-sm">{plat}</p>
+          
+          {!plat && !lancamento && <p className="text-white font-light text-sm">{rating}</p>}
+        </div>
+        <p className="text-white font-light text-sm">{lancamento}</p>
+      </div>
+    </div>
+  )
+}
