2b1c0fa05343f2b8bb6b30d959cc2ddcea285062
duarteeigor <duarteigor562@gmail.com>
2025-10-06 18:24:42 -0300

    styles: adjusting header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,42 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
 import logo from "../../../public/logo.png"
 
+import { usePathname } from "next/navigation"
+
 export default function Header(){
+    const pathname = usePathname()
+    const isGamePage = pathname.startsWith(`/games/`)
+
     return(
-        <header className="bg-[#121212] h-25 w-full flex justify-between items-center text-amber-50">
+        <header className={`h-25 w-full flex justify-between items-center text-amber-50 z-99 fixed top-0 ${isGamePage? "bg-transparent" : "bg-[#121212]" }`}>
             <div className="max-w-7xl w-full flex items-center justify-between mx-auto">
                 <Link  href={'/'}>
-                    <Image src={logo} width={220} height={50} alt="Logo Game Scope" className="object-left object-contain"/>
+                    <Image 
+                      src={logo} 
+                      width={220} 
+                      height={50} 
+                      alt="Logo Game Scope" 
+                      className="object-left object-contain"
+                    />
                 </Link>
 
             <nav className="flex gap-6">
-                <Link href={'/jogos'} className="hover:opacity-80">Jogos</Link>
+                <Link href={'/games'} className="hover:opacity-80">Jogos</Link>
                 <Link href={'/lancamentos'} className="hover:opacity-80">Lançamentos</Link>
             </nav>
             </div>
         </header>
     )
 }
+
+
+
+
+
+
+
+
+
