0e1949c2ef6492ccafe609c942dd1663dd60e1e7
duarteeigor <duarteigor562@gmail.com>
2025-10-06 22:42:45 -0300

    fix: fix the logo link overflowing the header content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,40 +3,39 @@
 import Link from "next/link"
 import Image from "next/image"
 import logo from "../../../public/logo.png"
-
 import { usePathname } from "next/navigation"
 
-export default function Header(){
-    const pathname = usePathname()
-    const isGamePage = pathname.startsWith(`/games/`)
+export default function Header() {
+  const pathname = usePathname()
+  const isGamePage = pathname.startsWith(`/games/`)
 
-    return(
-        <header className={`h-25 w-full flex justify-between items-center text-amber-50 z-99 fixed top-0 ${isGamePage? "bg-transparent" : "bg-[#121212]" }`}>
-            <div className="max-w-7xl w-full flex items-center justify-between mx-auto">
-                <Link  href={'/'}>
-                    <Image 
-                      src={logo} 
-                      width={220} 
-                      height={50} 
-                      alt="Logo Game Scope" 
-                      className="object-left object-contain"
-                    />
-                </Link>
+  return (
+    <header
+      className={`fixed top-0 left-0 w-full h-[100px] flex items-center z-50 ${
+        isGamePage ? "bg-transparent" : "bg-[#121212]"
+      }`}
+    >
+      <div className="max-w-7xl w-full flex items-center justify-between mx-auto px-4">
+        <div className="relative  w-[180px] h-[50px]">
+          <Link href="/">
+          <Image
+            src={logo}
+            alt="Logo Game Scope"
+            fill
+            className="object-cover"
+          />
+        </Link>
+        </div>
 
-            <nav className="flex gap-6">
-                <Link href={'/games'} className="hover:opacity-80">Jogos</Link>
-                <Link href={'/lancamentos'} className="hover:opacity-80">Lançamentos</Link>
-            </nav>
-            </div>
-        </header>
-    )
+        <nav className="flex gap-6 text-amber-50">
+          <Link href="/games" className="hover:opacity-80">
+            Jogos
+          </Link>
+          <Link href="/lancamentos" className="hover:opacity-80">
+            Lançamentos
+          </Link>
+        </nav>
+      </div>
+    </header>
+  )
 }
-
-
-
-
-
-
-
-
-
